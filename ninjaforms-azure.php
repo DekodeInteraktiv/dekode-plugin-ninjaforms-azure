e3879f6b884d95556156bfc1c97ec0ba67f91ab2
Ilya <ilya@ilya.ilya>
2021-08-27 11:30:20 +0300

    Prepare repository - cleanup

---
+++ ninjaforms-azure.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Ninja Forms - File Uploads to Azure
+ * Plugin URI:      https://dekode.no
+ * Description:
+ * Author:          Dekode
+ * Author URI:      https://dekode.no
+ * Text Domain:     dekode-ninjaforms-azure
+ * Version:         1.0.0
+ *
+ * @package DekodeNinjaFormsAzure
+ */
+
+declare( strict_types=1 );
+
+namespace Dekode\NinjaForms\Azure;
+
+define('DEKODE_NINJAFORMS_AZURE_VERSION', '1.0.0');
+define('DEKODE_NINJAFORMS_AZURE_DIR_PATH', plugin_dir_path(__FILE__));
--- tools/codeship/test-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/clone
-npm run lint && composer lint
