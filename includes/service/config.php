9b13a06f9b94cfebbcdf9737401ff31b6e6343a2
Marius Jensen <marius@clorith.net>
2021-09-01 10:38:51 +0200

    Ensure the array key, and field ID match for settings

--- includes/service/config.php
+++ includes/service/config.php
@@ -9,19 +9,19 @@ declare( strict_types=1 );
 
 return apply_filters( 'ninja_forms_uploads_settings_azure', [
 	'MICROSOFT_AZURE_ACCOUNT_NAME' => [
-		'id'      => 'azure_account_name',
+		'id'      => 'MICROSOFT_AZURE_ACCOUNT_NAME',
 		'type'    => 'textbox',
 		'default' => '',
 		'label'   => __( 'Account Name', 'dekode-ninjaforms-azure' ),
 	],
 	'MICROSOFT_AZURE_ACCOUNT_KEY'  => [
-		'id'      => 'azure_account_key',
+		'id'      => 'MICROSOFT_AZURE_ACCOUNT_KEY',
 		'type'    => 'textbox',
 		'default' => '',
 		'label'   => __( 'Account Key', 'dekode-ninjaforms-azure' ),
 	],
 	'MICROSOFT_AZURE_CONTAINER'    => [
-		'id'    => 'azure_blob_service_endpoint',
+		'id'    => 'MICROSOFT_AZURE_CONTAINER',
 		'type'  => 'textbox',
 		'label' => __( 'Blob Service Endpoint', 'dekode-ninjaforms-azure' ),
 		'desc'  => __( 'Service endpoint values in your connection strings must be well-formed URIs, including https:// (recommended) or http://.', 'dekode-ninjaforms-azure' ),
